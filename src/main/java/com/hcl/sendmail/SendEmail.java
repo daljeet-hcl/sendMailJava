be6fae94dca0f89bbfb226aeac618b0ebbc00be9
daljeet Singh <daljeets@hcl.com>
2016-10-23 12:33:44 +0530

    fixed java file

--- src/main/java/com/hcl/sendmail/SendEmail.java
+++ src/main/java/com/hcl/sendmail/SendEmail.java
@@ -1,32 +1,34 @@
-    import java.util.*;  
-    import javax.mail.*;  
-    import javax.mail.internet.*;  
-    import javax.activation.*;  
+package com.hcl.sendmail;
+
+import java.util.*;
+import javax.mail.*;  
+import javax.mail.internet.*;  
+import javax.activation.*;  
       
-    public class SendEmail  
-    {  
-     public static void main(String [] args){  
-          String to = "daljeets@hcl.com";//change accordingly  
-          String from = "ottadm@localhost";//change accordingly  
-          String host = "localhost";//or IP address  
+public class SendEmail  
+{  
+    public static void main(String [] args){  
+        String to = "daljeets@hcl.com";//change accordingly  
+        String from = "ottadm@localhost";//change accordingly  
+        String host = "localhost";//or IP address  
       
-         //Get the session object  
-          Properties properties = System.getProperties();  
-          properties.setProperty("mail.smtp.host", host);  
-          Session session = Session.getDefaultInstance(properties);  
+        //Get the session object  
+        Properties properties = System.getProperties();  
+        properties.setProperty("mail.smtp.host", host);  
+        Session session = Session.getDefaultInstance(properties);  
       
-         //compose the message  
-          try{  
-             MimeMessage message = new MimeMessage(session);  
-             message.setFrom(new InternetAddress(from));  
-             message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
-             message.setSubject("Ping");  
-             message.setText("Hello, this is example of sending email  ");  
+        //compose the message  
+        try{  
+            MimeMessage message = new MimeMessage(session);  
+            message.setFrom(new InternetAddress(from));  
+            message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+            message.setSubject("Ping");  
+            message.setText("Hello, this is example of sending email  ");  
       
-             // Send message  
-             Transport.send(message);  
-             System.out.println("message sent successfully....");  
+            // Send message  
+            Transport.send(message);  
+            System.out.println("message sent successfully....");  
       
-          }catch (MessagingException mex) {mex.printStackTrace();}  
-       }  
+        }catch (MessagingException mex) {mex.printStackTrace();}  
     }  
+}  
